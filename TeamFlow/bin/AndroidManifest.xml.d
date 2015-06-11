c9a0a632a5b7f884c1219027d8ab845c7f37a3d5
munwar24 <cm.munwar@gmail.com>
2015-06-11 17:34:13 +0530

    updated propetites for android target

---
+++ TeamFlow/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\deploys\android\local\android-local-tcflow-actor\staged-4.4.6\TeamFlow\bin\AndroidManifest.xml : \
+C:\deploys\android\local\android-local-tcflow-actor\staged-4.4.6\TeamFlow\AndroidManifest.xml \
---
+++ TeamFlow/gen/com/android/internal/telephony/ITelephony.java.d
@@ -0,0 +1,3 @@
+C:\deploys\android\local\android-local-tcflow-actor\staged-4.4.6\TeamFlow\gen\com\android\internal\telephony\ITelephony.java: \
+  C:\deploys\android\local\android-local-tcflow-actor\staged-4.4.6\TeamFlow\src\com\android\internal\telephony\ITelephony.aidl 
+
